055531d0e29df467ab7ed81dd664c91b893ef950
schaoss <ma.gary729@gmail.com>
2019-04-18 18:32:41 +0800

    update Containers

--- src/5 kyu/Containers/index.js
+++ src/5 kyu/Containers/index.js
@@ -1,8 +1,11 @@
 // < 5 kyu > - Containers
 // https://www.codewars.com/kata/5b80dea49895f71f3e00002d
 
+// const Containers = q =>
+//   q.split('').reduce((a, c) => {
+//     let i = a.findIndex(x => c <= x)
+//     return i > -1 ? ((a[i] = c), a) : (a.push(c), a)
+//   }, []).length
+
 const Containers = q =>
-  q.split('').reduce((a, c) => {
-    let i = a.findIndex(x => c <= x)
-    return i > -1 ? ((a[i] = c), a) : (a.push(c), a)
-  }, []).length
+  q.split('').reduce((a, c) => ((i = a.findIndex(x => c <= x)), i > -1 ? ((a[i] = c), a) : (a.push(c), a)), []).length
