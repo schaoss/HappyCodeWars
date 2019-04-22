a6878b50487ce71fbf3b69d774af9e734b96d051
schaoss <ma.gary729@gmail.com>
2019-04-22 17:03:59 +0800

    Check if two words are isomorphic to each other

--- src/6 kyu/CheckIfTwoWordsAreIsomorphicToEachOther/index.js
+++ src/6 kyu/CheckIfTwoWordsAreIsomorphicToEachOther/index.js
@@ -1,3 +1,15 @@
 // < 6 kyu > - Check if two words are isomorphic to each other
 // https://www.codewars.com/kata/check-if-two-words-are-isomorphic-to-each-other
 
+function isomorph(a, b) {
+  const ar1 = a.split('')
+  const ar2 = b.split('')
+  const map = {}
+
+  for (let i = 0; i < ar1.length; i++) {
+    if (!map[ar1[i]] && !Object.values(map).includes(ar2[i])) map[ar1[i]] = ar2[i]
+    if (ar2[i] === map[ar1[i]]) ar2[i] = ar1[i]
+  }
+
+  return ar1.join('') === ar2.join('')
+}
